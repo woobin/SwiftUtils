6fe61eee1fd3c82add8e5caeada45be33a90d349
Daikon Nagabuchi <jlee.woobin@gmail.com>
2024-07-09 22:01:28 +0900

    refactor

--- SendMailUtil.swift
+++ SendMailUtil.swift
@@ -9,72 +9,78 @@ import MessageUI
 
 class SendMailUtil: NSObject, MFMailComposeViewControllerDelegate {
     
-    class var shared : SendMailUtil {
-        struct Static {
-            static let instance : SendMailUtil = SendMailUtil()
-        }
-        return Static.instance
-    }
+    // シングルトンインスタンスの生成
+    static let shared = SendMailUtil()
+    
+    // プライベートイニシャライザ
     private override init() {}
 
-    var opener: UIViewController?
+    private weak var opener: UIViewController?
 
-    func sendFeedBack(opener: UIViewController) {
-        if MFMailComposeViewController.canSendMail()==false {
-            ViewUtil.shared.showAlert(message: "メール設定確認のお願い！",  actionTitles: ["はい"], actions: nil)
+    // フィードバックメールを送信するメソッド
+    func sendFeedback(from viewController: UIViewController) {
+        guard MFMailComposeViewController.canSendMail() else {
+            ViewUtil.shared.showAlert(message: "メール設定確認のお願い！", actionTitles: ["はい"], actions: nil)
             return
         }
-        self.opener = nil
-        self.opener = opener
         
-        let mailCompose = MFMailComposeViewController()
-        let destination = ["***@***.com"]
+        self.opener = viewController
         
-        mailCompose.mailComposeDelegate = self
-        mailCompose.setSubject("タイトル")
-        mailCompose.setToRecipients(destination)
+        let mailComposeVC = createMailComposeViewController()
         
-        mailCompose.setMessageBody(self.getMessageFeedback(), isHTML: false)
-        self.opener?.present(mailCompose, animated: true, completion: nil)
+        viewController.present(mailComposeVC, animated: true, completion: nil)
     }
 
+    // MARK: - MFMailComposeViewControllerDelegate
     
-    //MARK: MFMailComposeViewControllerDelegate
     func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
-
+        handleMailComposeResult(result, error: error)
+        opener?.dismiss(animated: true, completion: nil)
+    }
+    
+    // MARK: - プライベートメソッド
+    
+    private func createMailComposeViewController() -> MFMailComposeViewController {
+        let mailComposeVC = MFMailComposeViewController()
+        let recipients = ["***@***.com"]
+        
+        mailComposeVC.mailComposeDelegate = self
+        mailComposeVC.setSubject("タイトル")
+        mailComposeVC.setToRecipients(recipients)
+        mailComposeVC.setMessageBody(createFeedbackMessage(), isHTML: false)
+        
+        return mailComposeVC
+    }
+    
+    private func handleMailComposeResult(_ result: MFMailComposeResult, error: Error?) {
         switch result {
         case .cancelled:
-            break
+            print("メール送信がキャンセルされました")
         case .saved:
-            break
+            print("メールが保存されました")
         case .sent:
-            ViewUtil.shared.showAlert(message: "送信した！", actionTitles: ["確認"], actions: nil)
-            break
+            ViewUtil.shared.showAlert(message: "送信しました！", actionTitles: ["確認"], actions: nil)
         case .failed:
-            var message = "失敗した！"
-            if let err: String = error?.localizedDescription, err.count() > 0 {
-                message.append("\n")
-                message.append(err)
+            var message = "送信に失敗しました！"
+            if let errorDescription = error?.localizedDescription, !errorDescription.isEmpty {
+                message.append("\n\(errorDescription)")
             }
             ViewUtil.shared.showAlert(message: message, actionTitles: ["確認"], actions: nil)
-            break
+        @unknown default:
+            fatalError("予期しないケースです")
         }
-        self.opener?.dismiss(animated: true, completion: nil)
     }
     
-    //MARK: Create Message
-    private func getMessageFeedback() -> String {
+    private func createFeedbackMessage() -> String {
+        let device = UIDevice.current
+        let bundle = Bundle.main
+        let locale = Locale.current.identifier
+        let appName = bundle.object(forInfoDictionaryKey: "CFBundleName") as? String ?? "アプリ名不明"
+        let appVersion = bundle.infoDictionary?["CFBundleShortVersionString"] as? String ?? "バージョン不明"
 
-        let dc = UIDevice.current
-        let bd = Bundle.main
-        let lc = Locale.current.identifier
-        let name = bd.object(forInfoDictionaryKey: "CFBundleName")
-        let appInfo  = "\n \(name!) \(bd.appVersion)\n \(dc.systemName) \(dc.systemVersion)\n \(dc.model), \(lc)"
-
+        let appInfo = "\n\(appName) \(appVersion)\n\(device.systemName) \(device.systemVersion)\n\(device.model), \(locale)"
         let message = "何か書きます。\n\n\n\n\n+---------------+\(appInfo)\n+---------------+"
-    
+        
         return message
     }
-
-    
 }
